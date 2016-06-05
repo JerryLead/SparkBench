5e7c9d15d7f42e22f069759f16232a88f5d9ac59
JerryLead <JerryLead@163.com>
2016-06-05 11:33:55 +0800

    Current pom.xml cannot pass the yarn client test

--- src/main/scala/yarn/cluster/WordCount.scala
+++ src/main/scala/yarn/cluster/WordCount.scala
@@ -3,21 +3,25 @@ package yarn.cluster
 import org.apache.spark.{SparkContext, SparkConf}
 
 /**
+  * YARN HADOOP_CONF_DIR
   * Created by lijie on 16-6-2.
   * --class yarn.cluster.WordCount --master yarn --deploy-mode client --executor-memory 2g --executor-cores 2 --queue experiments target/SparkBench-1.0-SNAPSHOT.jar
   */
 object WordCount {
   def main(args: Array[String]) {
+
     val conf = new SparkConf().setAppName("WordCount")
-    // conf.setMaster("local")
+    // conf.setMaster("local-cluster[2, 1, 2048]")
     val sc = new SparkContext(conf)
 
-    val filePath = "src/main/scala/yarn/cluster/WordCount.scala"
+    // val filePath = "src/main/scala/yarn/cluster/WordCount.scala"
+    val filePath = "hdfs://master:9000/user/lijie/input"
     val textFile = sc.textFile(filePath)
     val result = textFile.flatMap(_.split("[ |\\.]"))
       .map(word => (word, 1)).reduceByKey(_ + _)
 
     result.foreach(println)
+    sc.stop()
   }
 
 }

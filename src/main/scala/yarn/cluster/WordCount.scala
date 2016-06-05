199afc4652a7de62b2c42f4a639f1cd3ae4603fd
JerryLead <JerryLead@163.com>
2016-06-05 21:10:55 +0800

    Can run wordcount on YARN with dynamic allocation

--- src/main/scala/yarn/cluster/WordCount.scala
+++ src/main/scala/yarn/cluster/WordCount.scala
@@ -12,10 +12,12 @@ object WordCount {
   def main(args: Array[String]) {
 
     val conf = new SparkConf().setAppName("WordCount")
+    /*
     conf.set("spark.yarn.jar", "hdfs://master:9000/spark_lib/spark-assembly-1.6.2-SNAPSHOT-hadoop2.6.4.jar")
     conf.set("spark.eventLog.enabled", "true")
     conf.set("spark.eventLog.dir", "hdfs://master:9000/sparklogs")
     conf.set("spark.yarn.historyServer.address", "master:18080")
+    */
     // conf.setMaster("local-cluster[2, 1, 2048]")
     val sc = new SparkContext(conf)
 
